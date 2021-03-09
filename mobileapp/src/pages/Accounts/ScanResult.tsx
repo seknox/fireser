2bdd490adf6bee1b61af9d5eeaa4e5f747573a17
sakshyam shah <sakshyam.shah@gmail.com>
2021-03-09 13:07:53 +0545

    wip: scan page ui with data

--- mobileapp/src/pages/Accounts/ScanResult.tsx
+++ mobileapp/src/pages/Accounts/ScanResult.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2020 Seknox Pte Ltd.
+ * Copyright (C) 2020-2021 Seknox Pte Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -15,9 +15,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {Button, Divider, List, ListItem, Text} from '@ui-kitten/components';
+import { Button, Divider, List, ListItem, Text } from '@ui-kitten/components';
 import React, { useEffect } from 'react';
-import { Pressable, View, ScrollView } from 'react-native';
+import { Pressable, View, ScrollView, Image } from 'react-native';
 import Layout from '../../components/Layout';
 
 import { StyleService, useStyleSheet } from '@ui-kitten/components';
@@ -92,6 +92,13 @@ const themedStyles = StyleService.create({
     flexWrap: 'wrap',
     marginVertical: 15,
   },
+  headerText: {
+    flex: 2,
+    marginHorizontal: 15,
+    fontSize: 19,
+    flexWrap: 'wrap',
+    marginVertical: 15,
+  },
   button: {
     flex: 1,
     justifyContent: 'flex-start',
@@ -116,12 +123,17 @@ const themedStyles = StyleService.create({
     // resizeMode: 'contain',
   },
 
-  card2: {
+  connectedAccCard: {
     flex: 1,
-    minHeight: 130,
-    marginVertical: 5,
-    marginHorizontal: 20,
-    flexDirection: 'row',
+    padding: 10,
+  },
+  tinyBox: {
+    flex: 1,
+    flexDirection: 'column',
+    minHeight: 120,
+    minWidth: 100,
+    marginHorizontal: 5,
+    marginVertical: 20,
     backgroundColor: 'white',
     borderRadius: 8,
     shadowColor: '#000',
@@ -132,6 +144,19 @@ const themedStyles = StyleService.create({
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
   },
+  deviceLogo: {
+    flex: 2,
+    width: 80,
+    height: 50,
+  },
+  tinyText: {
+    flex: 1,
+  },
+  appLogo: {
+    flex: 2,
+    width: 50,
+    height: 50,
+  },
 });
 
 type accountProps = {
@@ -140,6 +165,26 @@ type accountProps = {
 
 export default function ScanResults(props: any) {
   const styles = useStyleSheet(themedStyles);
+
+  const { connectedDevices, thirdPartyApps } = props.result;
+
+  function getDevices(dType: string, imgURL: string) {
+    switch (dType) {
+      case 'android':
+        return imgURL;
+      case 'Linux':
+        return 'https://storage.googleapis.com/fireser-app-public-assets/icons/devices/linuxlaptop.png';
+      case 'iphone':
+        return 'https://storage.googleapis.com/fireser-app-public-assets/icons/devices/iphone-x-icon.png';
+      case 'Mac':
+        return 'https://storage.googleapis.com/fireser-app-public-assets/icons/devices/mac.png';
+      case 'Windows':
+        return 'https://storage.googleapis.com/fireser-app-public-assets/icons/devices/winlaptop.png';
+      default:
+        return 'https://www.gstatic.com/identity/boq/accountsettingssecuritycommon/images/sprites/devices_realistic_72-ef7d73f742f5343ba1bd3c1e6b13ffba.png';
+    }
+  }
+
   return (
     <View>
       <View style={styles.root}>
@@ -157,6 +202,7 @@ export default function ScanResults(props: any) {
             </Text>
           </Pressable>
         </View>
+        <Divider />
         <View style={styles.iconButton}>
           <Pressable
             // onPress={() => navigation.navigate('ScanAndProtect', { name: 'Google' })}
@@ -173,24 +219,62 @@ export default function ScanResults(props: any) {
         </View>
       </View>
 
-      <View style={styles.card2}>
-        <Text category="h5">Your Devices </Text>
-        {/*{props.result.connectedDevices.map((d, i) => (*/}
-        {/*    //TODO @sshahcodes create list from props.result.connectedDevices*/}
-        {/*  // <List key={i}>*/}
-        {/*  //   <ListItem>{d.title}</ListItem>*/}
-        {/*  //   <ListItem>{d.name}</ListItem>*/}
-        {/*  //   <ListItem>{d.os}</ListItem>*/}
-        {/*  // </List>*/}
-        {/*))}*/}
+      <View style={styles.connectedAccCard}>
+        <Text category="s1" style={styles.headerText}>
+          {connectedDevices?.length} devices connected to this account{' '}
+        </Text>
+
+        <ScrollView horizontal={true}>
+          {connectedDevices.map((d: any, i: number) => (
+            <View key={i} style={styles.tinyBox}>
+              <Pressable
+              // onPress={() => navigation.navigate('ScanAndProtect', { name: 'Google' })}
+              // style={styles.touch}
+              >
+                <Image
+                  style={styles.deviceLogo}
+                  source={{
+                    uri: d.imgURL ? d.imgURL : getDevices(d.title, d.imgURL),
+                  }}
+                />
+
+                <Text category="s1" style={styles.tinyText}>
+                  {d.title}
+                </Text>
+              </Pressable>
+            </View>
+          ))}
+        </ScrollView>
       </View>
 
-      <View style={styles.card2}>
-        <Text category="h5">Connected Apps</Text>
-        {/*TODO @sshahcodes create list from props.result.connectedApps */}
+      <View style={styles.connectedAccCard}>
+        <Text category="s1" style={styles.headerText}>
+          {thirdPartyApps?.length} 3rd party apps have access to your data{' '}
+        </Text>
+        <ScrollView horizontal={true}>
+          {thirdPartyApps.map((d: any, i: number) => (
+            <View style={styles.tinyBox} key={i}>
+              <Pressable
+              // onPress={() => navigation.navigate('ScanAndProtect', { name: 'Google' })}
+              // style={styles.touch}
+              >
+                <Image
+                  style={styles.appLogo}
+                  source={{
+                    uri: d.imgURL,
+                  }}
+                />
+
+                <Text category="s1" style={styles.tinyText}>
+                  {d.name}
+                </Text>
+              </Pressable>
+            </View>
+          ))}
+        </ScrollView>
       </View>
 
-      <View style={styles.card}>
+      {/* <View style={styles.card}>
         <Pressable onPress={props.navigateToAccounts} style={styles.touch}>
           <View style={styles.desc}>
             <Text category="h5">Your Public Profile</Text>
@@ -205,9 +289,9 @@ export default function ScanResults(props: any) {
             </Button>
           </View>
 
-          <View style={styles.imageContainer}>{/* <AccountSec style={styles.image} /> */}</View>
+          <View style={styles.imageContainer}><AccountSec style={styles.image} /></View>
         </Pressable>
-      </View>
+      </View> */}
     </View>
   );
 }
